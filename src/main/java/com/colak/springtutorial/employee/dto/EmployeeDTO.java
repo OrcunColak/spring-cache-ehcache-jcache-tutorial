df3e880ae5fc4b23b7c946577aff617e1e15654d
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-19 09:50:18 +0300

    offheap example

--- src/main/java/com/colak/springtutorial/employee/dto/EmployeeDTO.java
+++ src/main/java/com/colak/springtutorial/employee/dto/EmployeeDTO.java
@@ -3,9 +3,16 @@ package com.colak.springtutorial.employee.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+
+// Serializable since ehcache config uses offheap configuration
 @Getter
 @Setter
-public class EmployeeDTO {
+public class EmployeeDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private Long id;
 
